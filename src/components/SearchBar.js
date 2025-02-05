7e2c055dc4cb8fb5816d53cfd2fe2b6ede295d45
Jeff Sokol <JeffSokolDev@gmail.com>
2025-02-05 16:58:12 -0500

    start of results

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,27 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import SearchResults from "./SearchResults";
 import { musicSearch } from "./data/music";
 
-let musicData = {name:'Sokol'}
 
-async function handleSearch(){
-    let value = document.getElementById('searchField').value
-    alert(`searching ${value}`)
-    musicData =  await musicSearch(value)
-    console.log(`musicData ${musicData}`)
-    return musicData
-}
 
 export default function SearchBar(){
+    const [music, setMusic] = useState(null)
+    const musicData = async () => { 
+        let value = document.getElementById('searchField').value
+        alert(`searching ${value}`)
+        const data =  await musicSearch(value)
+        setMusic(data)
+        console.log('musicSearch', music)
+        return music
+
+}
     return(
         <>
         <div className="searchBarDiv">
             <h2>Please Enter an Artist</h2>
             <input type="text" id="searchField" placeholder="Artist Name..."/>
-            <input type="button" value="Search" onClick={ handleSearch }/>
+            <input type="button" value="Search" onClick={ musicData }/>
         </div>
-        <SearchResults data={musicData}/>
+        <SearchResults props={music}/>
         </>
     )
 }
--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,10 +1,19 @@
+import { useState } from "react"
+
 export default function SearchResults(props){
-    console.log('default', props.data.name)
-    console.log('after', props)
+    console.log('results', props)
+
+    if(!props.musicData){
+        return(
+            <>
+                <h4>Nothing Found</h4>
+            </>
+        )
+    }else
     return(
         <>
-        <h4>search results</h4>
-        <p>hello, {props.data.name}</p>
+            <h4>search results</h4>
+            <p>hello</p>
         </>
     )
 }
